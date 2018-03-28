4588a8c3bbfa902d24f9f8527c1adeddf52ae2b5
Konstantin8105 <konstantin8105@gmail.com>
2018-03-28 14:54:20 +0300

    separate to sqlite

--- travis/test.sh
+++ travis/test.sh
@@ -67,52 +67,9 @@ if [ $(cat /tmp/prime.go | wc -l) -eq 0 ]; then exit 1; fi
 if [ $($C4GO ast $C4GO_DIR/examples/prime.c | wc -l) -eq 0 ]; then exit 1; fi
 
 echo "----------------------"
-# This will have to be updated every so often to the latest version. You can
-# find the latest version here: https://sqlite.org/download.html
-export SQLITE3_FILE=sqlite-amalgamation-3220000
 
-# Variable for location of temp sqlite files
-SQLITE_TEMP_FOLDER="/tmp/SQLITE"
-mkdir -p $SQLITE_TEMP_FOLDER
-
-# Download/unpack SQLite if required.
-if [ ! -e $SQLITE_TEMP_FOLDER/$SQLITE3_FILE.zip ]; then
-    curl http://sqlite.org/2018/$SQLITE3_FILE.zip > $SQLITE_TEMP_FOLDER/$SQLITE3_FILE.zip
-    unzip $SQLITE_TEMP_FOLDER/$SQLITE3_FILE.zip -d $SQLITE_TEMP_FOLDER
-fi
-
-# Clean generated files. This should not be required, but it's polite.
-rm -f $SQLITE_TEMP_FOLDER/sqlite3.go $SQLITE_TEMP_FOLDER/shell.go
-
-# Transpile the SQLite3 files.
-# If transpiling write to stderr, then it will be append into OUTFILE
-# shell.c
-echo "Transpiling shell.c..."
-./c4go transpile -o=$SQLITE_TEMP_FOLDER/shell.go   $SQLITE_TEMP_FOLDER/$SQLITE3_FILE/shell.c   >> $OUTFILE 2>&1
-
-# sqlite3.c
-echo "Transpiling sqlite3.c..."
-./c4go transpile -o=$SQLITE_TEMP_FOLDER/sqlite3.go $SQLITE_TEMP_FOLDER/$SQLITE3_FILE/sqlite3.c >> $OUTFILE 2>&1
-
-# Show amount "Warning" in sqlite Go codes
-SQLITE_WARNINGS=`cat $SQLITE_TEMP_FOLDER/sqlite3.go $SQLITE_TEMP_FOLDER/shell.go | grep "// Warning" | wc -l`
-echo "In files (sqlite3.go and shell.go) summary : $SQLITE_WARNINGS warnings."
-
-# SQLITE
-c4go transpile -o="$SQLITE_TEMP_FOLDER/sqlite.go" -clang-flag="-DSQLITE_THREADSAFE=0" -clang-flag="-DSQLITE_OMIT_LOAD_EXTENSION" $SQLITE_TEMP_FOLDER/$SQLITE3_FILE/shell.c $SQLITE_TEMP_FOLDER/$SQLITE3_FILE/sqlite3.c
-
-# Show amount "Warning":
-SQLITE_WARNINGS=`cat $SQLITE_TEMP_FOLDER/sqlite.go | grep "// Warning" | wc -l`
-echo "After transpiling shell.c and sqlite3.c together, have summary: $SQLITE_WARNINGS warnings."
-
-# Show amount error from `go build`:
-SQLITE_WARNINGS_GO=`go build $SQLITE_TEMP_FOLDER/sqlite.go 2>&1 | wc -l`
-echo "In file sqlite.go summary : $SQLITE_WARNINGS_GO warnings in go build."
-
-# Amount warning from gometalinter
-echo "Calculation warnings by gometalinter"
-GOMETALINTER_WARNINGS=`$GOPATH/bin/gometalinter $SQLITE_TEMP_FOLDER/sqlite.go 2>&1 | wc -l`
-echo "Amount found warnings by gometalinter at 30 second : $GOMETALINTER_WARNINGS warnings."
+# Run script sqlite
+source ./travis/sqlite.sh
 
 # Run script triangle
 source ./travis/triangle.sh
