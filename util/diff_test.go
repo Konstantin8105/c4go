bcafb71d043106642407f2e8f9d185bca21f5004
Konstantin8105 <konstantin8105@gmail.com>
2018-03-28 13:47:58 +0300

    add test for ShowDiff

---
+++ util/diff_test.go
@@ -0,0 +1,41 @@
+package util_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/elliotchance/c2go/util"
+)
+
+func TestDiff(t *testing.T) {
+	tcs := []struct {
+		a string
+		b string
+
+		expect string
+	}{
+		{
+			a:      "a",
+			b:      "b",
+			expect: "*   1 \"a\"                                     \"b\"",
+		},
+		{
+			a:      "a",
+			b:      "a",
+			expect: "1 \"a\"                                     \"a\"",
+		},
+	}
+
+	for i, tc := range tcs {
+		t.Run(fmt.Sprintf("Test %d : %s", i, tc.a+tc.b), func(t *testing.T) {
+			act := util.ShowDiff(tc.a, tc.b)
+			if strings.TrimSpace(act) != strings.TrimSpace(tc.expect) {
+				t.Errorf("Not correct result.\nExpected:%s\nActual:%s\n",
+					tc.expect, act)
+				t.Errorf("Length\nExpected:%d\nActual:%d\n",
+					len(tc.expect), len(act))
+			}
+		})
+	}
+}
