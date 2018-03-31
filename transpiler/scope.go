6ebcf082d1ff07c1a892ece3a7f5d7d9faf5d33f
Konstantin <konstantin8105@gmail.com>
2018-04-01 00:34:25 +0300

    va_list with pointer type (#25)

--- transpiler/scope.go
+++ transpiler/scope.go
@@ -6,21 +6,54 @@ package transpiler
 import (
 	"fmt"
 	goast "go/ast"
+	"go/token"
 
 	"github.com/Konstantin8105/c4go/ast"
 	"github.com/Konstantin8105/c4go/program"
 )
 
 func transpileCompoundStmt(n *ast.CompoundStmt, p *program.Program) (
-	*goast.BlockStmt, []goast.Stmt, []goast.Stmt, error) {
-	preStmts := []goast.Stmt{}
-	postStmts := []goast.Stmt{}
+	_ *goast.BlockStmt, preStmts []goast.Stmt, postStmts []goast.Stmt, err error) {
 	stmts := []goast.Stmt{}
 
 	for _, x := range n.Children() {
-		result, err := transpileToStmts(x, p)
-		if err != nil {
-			return nil, nil, nil, err
+		// Implementation va_start
+		var isVaList bool
+		if call, ok := x.(*ast.CallExpr); ok && call.Type == "void" {
+			if impl, ok := call.Children()[0].(*ast.ImplicitCastExpr); ok {
+				if impl.Type == "void (*)(struct __va_list_tag *, ...)" {
+					if decl, ok := impl.Children()[0].(*ast.DeclRefExpr); ok {
+						if decl.Name == "__builtin_va_start" {
+							isVaList = true
+						}
+					}
+				}
+			}
+		}
+
+		var result []goast.Stmt
+		if isVaList {
+			// Implementation va_start
+			result = []goast.Stmt{
+				&goast.AssignStmt{
+					Lhs: []goast.Expr{
+						goast.NewIdent("c4goVaListPosition"),
+					},
+					Tok: token.ASSIGN,
+					Rhs: []goast.Expr{
+						&goast.BasicLit{
+							Kind:  token.INT,
+							Value: "0",
+						},
+					},
+				},
+			}
+		} else {
+			// Other cases
+			result, err = transpileToStmts(x, p)
+			if err != nil {
+				return nil, nil, nil, err
+			}
 		}
 
 		if result != nil {
