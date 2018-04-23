6e616355f49cdf42d9beace66a2f09eebb4aafb9
Konstantin8105 <konstantin8105@gmail.com>
2018-04-23 16:28:43 +0300

    change test

--- ast/compound_literal_expr_test.go
+++ ast/compound_literal_expr_test.go
@@ -11,7 +11,7 @@ func TestCompoundLiteralExpr(t *testing.T) {
 			Pos:        NewPositionFromString("col:21, col:40"),
 			Type1:      "struct node",
 			Type2:      "struct node",
-			Lvalue:     false,
+			Lvalue:     true,
 			ChildNodes: []Node{},
 		},
 		`0x350b398 <col:24, col:31> '__CONST_SOCKADDR_ARG':'__CONST_SOCKADDR_ARG'`: &CompoundLiteralExpr{
@@ -19,7 +19,7 @@ func TestCompoundLiteralExpr(t *testing.T) {
 			Pos:        NewPositionFromString("col:24, col:31"),
 			Type1:      "__CONST_SOCKADDR_ARG",
 			Type2:      "__CONST_SOCKADDR_ARG",
-			Lvalue:     true,
+			Lvalue:     false,
 			ChildNodes: []Node{},
 		},
 	}
