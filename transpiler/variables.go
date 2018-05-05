f6515fe2cbc3f2d5f4c1e0144688e44ed20cb49b
Konstantin8105 <konstantin8105@gmail.com>
2018-05-05 19:10:24 +0300

    Function type for vars

--- transpiler/variables.go
+++ transpiler/variables.go
@@ -53,6 +53,19 @@ func transpileDeclRefExpr(n *ast.DeclRefExpr, p *program.Program) (
 		}
 	}
 
+	if n.For == "Function" {
+		var includeFile string
+		includeFile, err = p.GetIncludeFileNameByFunctionSignature(n.Name, n.Type)
+		p.AddMessage(p.GenerateWarningMessage(err, n))
+		if includeFile != "" && p.IncludeHeaderIsExists(includeFile) {
+			name := p.GetFunctionDefinition(n.Name).Substitution
+			if strings.Contains(name, ".") {
+				p.AddImport(strings.Split(name, ".")[0])
+			}
+			return goast.NewIdent(name), n.Type, nil
+		}
+	}
+
 	theType := n.Type
 
 	// FIXME: This is for linux to make sure the globals have the right type.
