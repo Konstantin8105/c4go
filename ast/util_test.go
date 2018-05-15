2ff0cc82ffe95b91f8ae5778437b87f6f5f78243
Konstantin <konstantin8105@gmail.com>
2018-05-15 14:00:10 +0300

    AST: add tests (#113)
    * Gofmt: simplification
    
    * add tests

---
+++ ast/util_test.go
@@ -0,0 +1,37 @@
+package ast
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRemoveQuotes(t *testing.T) {
+	tcs := []struct {
+		in  string
+		out string
+	}{
+		{"  ", ""},
+		{"  \"\"  ", ""},
+		{"''", ""},
+
+		{"  \"string\"  ", "string"},
+		{"'string'", "string"},
+	}
+	for index, tc := range tcs {
+		t.Run(fmt.Sprintf("%v", index), func(t *testing.T) {
+			a := removeQuotes(tc.in)
+			if a != tc.out {
+				t.Errorf("Not acceptable : `%v` `%v`", a, tc.out)
+			}
+		})
+	}
+}
+
+func TestAtof(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Not acceptable result")
+		}
+	}()
+	_ = atof("Some not float64")
+}
