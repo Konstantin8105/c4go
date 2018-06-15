ed10e30c60f899aecfcab9a1c1632f9fec5f856d
Konstantin8105 <konstantin8105@gmail.com>
2018-06-15 10:34:56 +0300

    fix math test

--- preprocessor/compiler.go
+++ preprocessor/compiler.go
@@ -2,7 +2,7 @@ package preprocessor
 
 func Compiler(isCPP bool) (compiler, compilerFlag string) {
 	compiler = "clang"
-	compilerFlag = "-std=c99"
+	compilerFlag = "" // "-std=c99"
 	if isCPP {
 		compiler = "clang++"
 		compilerFlag = "-std=c++98"
