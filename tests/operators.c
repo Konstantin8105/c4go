e4320f05b9344bff0af4a6a34d9a25d7b1ce4056
Konstantin8105 <konstantin8105@gmail.com>
2018-09-10 21:16:17 +0300

    add test

--- tests/operators.c
+++ tests/operators.c
@@ -42,7 +42,7 @@ double* return_null()
 
 int main()
 {
-    plan(98);
+    plan(100);
 
     int i = 10;
     signed char j = 1;
@@ -408,6 +408,14 @@ int main()
 		v = buf + (prefix != 0);
 		is_not_null(v);
 		is_streq(v,"ext");
+	}
+	
+	diag("Bitwise complement operator ~");
+	{
+		int i = 35;
+		int o = ~(i);
+		is_eq(o,-36);
+		is_eq(~-12,11);
 	}
 
     done_testing();
