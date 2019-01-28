e59824f32872c92b28ac826801ae90493aab1135
Konstantin8105 <konstantin8105@gmail.com>
2019-01-28 11:57:54 +0300

    golint: ignore vendor folder

--- travis/lint.sh
+++ travis/lint.sh
@@ -2,9 +2,17 @@
 
 set -e
 
-# Check go fmt first
-if [ -n "$(gofmt -l .)" ]; then
-    echo "Go code is not properly formatted. Use 'gofmt'."
-    gofmt -d .
-    exit 1
-fi
+mkdir -p ./testdata/
+
+file="./testdata/gofmt.list"
+eval "find ./ -name '*.go' | grep -v 'testdata' | grep -v 'vendor' > $file"
+
+while IFS= read -r line
+do
+	# Check go fmt first
+	if [ -n "$(gofmt -l $line)" ]; then
+		echo "Go code is not properly formatted. Use 'gofmt' for: $line"
+		gofmt -d .
+		exit 1
+	fi
+done < "$file"
