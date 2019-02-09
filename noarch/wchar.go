b1ec21bafb798bb293677af36151afc5bf341e45
Konstantin8105 <konstantin8105@gmail.com>
2019-02-09 20:27:57 +0300

    Wcscmp.Add rune in ast

--- noarch/wchar.go
+++ noarch/wchar.go
@@ -1,3 +1,20 @@
 package noarch
 
+import "strings"
+
 type WcharT = rune
+
+func Wcscmp(w1, w2 []WcharT) int {
+	if len(w1) > len(w2) {
+		return -1
+	}
+	if len(w1) < len(w2) {
+		return 1
+	}
+	for i := range w1 {
+		if r := strings.Compare(string(w1[i]), string(w2[i])); r != 0 {
+			return r
+		}
+	}
+	return 0
+}
