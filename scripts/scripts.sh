275211f14acd144c5efe127d4e4af2bbab5a21da
Konstantin8105 <konstantin8105@gmail.com>
2019-03-15 22:54:07 +0300

    add scripts

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -5,4 +5,9 @@ set -e
 ./scripts/9wm.sh
 # ./scripts/cis71.sh
 ./scripts/ed.sh
-./scripts/neat.sh
+./scripts/neateqn.sh
+./scripts/neatmkfn.sh
+./scripts/neatpost.sh
+./scripts/neatrefer.sh
+./scripts/neatroff.sh
+./scripts/neatvi.sh
