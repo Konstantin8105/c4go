204cc1e74ca537152afbfde3d9e5d83e79731d05
Konstantin8105 <konstantin8105@gmail.com>
2019-03-20 10:12:38 +0300

    formatting example C code

--- examples/math.c
+++ examples/math.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main() 
+int main()
 {
-	int n;
-	double param = 8.0, result;
-	result = frexp(param,&n);
-	printf("result = %5.2f\n", result);
-	printf("n      = %d\n"   , n     );
-	return 0;
+    int n;
+    double param = 8.0, result;
+    result = frexp(param, &n);
+    printf("result = %5.2f\n", result);
+    printf("n      = %d\n", n);
+    return 0;
 }
