bc32d72f57ed23c39cdf9ea6646bbcd57543f309
Konstantin8105 <konstantin8105@gmail.com>
2019-03-28 11:44:42 +0300

    create a new test init.c

--- tests/array.c
+++ tests/array.c
@@ -714,51 +714,6 @@ void test_matrix_init()
 	}
 }
 
-
-static char *kmap_fa[256] = {
-	[0] = "fa",
-	['`'] = "‍",
-	['1'] = "۱",
-};
-
-static char *digraphs[][2] = {
-	{"cq", "’"},
-	{"pl", "+"},
-	{"hy", "-"},
-	{"sl", "/"},
-};
-
-static int xai, xaw;
-
-static struct option {
-	char *abbr;
-	char *name;
-	int *var;
-} options[] = {
-	{"ai", "autoindent", &xai},
-	{"aw", "autowrite", &xaw},
-};
-
-int ec_print(char *s){
-	printf("%s\n",s);
-	return 32;
-}
-
-int ec_insert(char *s){
-	printf("%s\n",s);
-	return 42;
-}
-
-static struct excmd {
-	char *abbr;
-	char *name;
-	int (*ec)(char *s);
-} excmds[] = {
-	{"p", "print", ec_print},
-	{"a", "append", ec_insert},
-};
-
-
 int main()
 {
     plan(194);
