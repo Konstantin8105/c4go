2bc03a6ca6c9b04f9a6718b699b92b9a351f2fa4
Konstantin8105 <konstantin8105@gmail.com>
2019-04-02 16:24:53 +0300

    prepare

--- transpiler/gofmt.go
+++ transpiler/gofmt.go
@@ -27,20 +27,44 @@ import (
 //		13,
 // )
 
+const maxLineSymbol int = 100
+
 func addBreaklines(src string) string {
+	// separate long lines by comma
+	lines := strings.Split(src, "\n")
+	for i := range lines {
+		line := lines[i]
+		if len(line) > maxLineSymbol {
+			line = formatting(line)
+		}
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+func formatting(line string) string {
 	// parens
 	// * ()
 	// * []
 	// * {}
 	// * ""
+	parens := []struct {
+		name     string
+		from, to byte
+	}{
+		{"p1", '(', ')'},
+		{"p2", '[', ']'},
+		{"p3", '{', '}'},
+		{"p4", '"', '"'},
+	}
 
-	// separate long lines by comma
-	// lines := strings.Split(src, "\n")
-	// for i := range lines {
-	// 	if len(lines[i]) > 100 {
-	// 		fmt.Fprintf(os.Stdout, "%s\n", lines[i])
-	// 	}
+	// check last byte
+	if line[len(line)-1] != '}' || line[len(line)-1] != ')' {
+		return line
+	}
+
+	// for i := len(line) - 1; i >= 0; i-- {
+	//
 	// }
 
-	return src
 }
