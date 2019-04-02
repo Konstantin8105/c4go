6111d638d8e562826c8bb642c139df796f79dd5b
Konstantin8105 <konstantin8105@gmail.com>
2019-04-02 13:26:41 +0300

    add function in test

--- noarch/noarch.go
+++ noarch/noarch.go
@@ -1,5 +1,7 @@
 package noarch
 
+import "fmt"
+
 // BoolToInt converts boolean value to an int, which is a common operation in C.
 // 0 and 1 represent false and true respectively.
 func BoolToInt(x bool) int32 {
@@ -10,22 +12,17 @@ func BoolToInt(x bool) int32 {
 	return 0
 }
 
-// NotInt performs a logical not (!) on an integer and returns an integer.
-func NotInt(x int32) int32 {
-	if x == 0 {
-		return 1
+// Not performs a logical not (!) on an integer and returns an integer.
+func Not(x interface{}) int32 {
+	switch v := x.(type) {
+	case int, int8, int16, int32, int64:
+		return BoolToInt(v != 0)
+	case uint, uint8, uint16, uint32, uint64:
+		return BoolToInt(v != 0)
+	case float32, float64:
+		return BoolToInt(v != 0)
 	}
-
-	return 0
-}
-
-// NotInt32 performs a logical not (!) on an integer and returns an integer.
-func NotInt32(x int32) int32 {
-	if x == 0 {
-		return 1
-	}
-
-	return 0
+	panic(fmt.Errorf("not support type %T", x))
 }
 
 // NotUint16 performs a logical not (!) on an integer and returns an integer.
