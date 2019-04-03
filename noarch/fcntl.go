e751ea1a7da2a50db7c2e021c8e260ad57499ccf
Konstantin8105 <konstantin8105@gmail.com>
2019-04-03 12:06:54 +0300

    unix -> syscall

--- noarch/fcntl.go
+++ noarch/fcntl.go
@@ -1,14 +1,14 @@
 package noarch
 
-import "golang.org/x/sys/unix"
+import "syscall"
 
-type Flock = unix.Flock_t
+type Flock = syscall.Flock_t
 
 func Open(pathname []byte, flags int32, mode ...int32) int32 {
 	if len(mode) == 0 {
 		mode = append(mode, 644)
 	}
-	fd, err := unix.Open(CStringToString(pathname), int(flags), uint32(mode[0]))
+	fd, err := syscall.Open(CStringToString(pathname), int(flags), uint32(mode[0]))
 	if err != nil {
 		return -1
 	}
