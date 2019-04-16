c52ad130bc93e1947752fcc8e90d981ea5b20f4a
Konstantin8105 <konstantin8105@gmail.com>
2019-04-16 14:29:42 +0300

    create files with debug information

--- debug.go
+++ debug.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/Konstantin8105/c4go/ast"
 	"github.com/Konstantin8105/c4go/preprocessor"
@@ -48,7 +52,8 @@ func generateDebugCCode(args ProgramArgs, lines []string, filePP preprocessor.Fi
 		pos  ast.Position
 	}
 
-	var funcPoses []funcPos
+	// map[filename] []funcPos
+	funcPoses := map[string][]funcPos{}
 
 	for i := range tree {
 		tr, ok := tree[i].(*ast.TranslationUnitDecl)
@@ -78,23 +83,88 @@ func generateDebugCCode(args ProgramArgs, lines []string, filePP preprocessor.Fi
 				fmt.Fprintf(os.Stdout, "find function : %s\n", fd.Name)
 			}
 
-			funcPoses = append(funcPoses, funcPos{
+			// save position
+			f := funcPos{
 				name: fd.Name,
 				pos:  mst.Position(),
-			})
+			}
+			if sl, ok := funcPoses[mst.Position().File]; ok {
+				sl = append(sl, f)
+				funcPoses[mst.Position().File] = sl
+				continue
+			}
+			funcPoses[mst.Position().File] = append([]funcPos{}, f)
 		}
 	}
 
 	if args.verbose {
-		fmt.Fprintf(os.Stdout, "found %d functions\n", len(funcPoses))
+		fmt.Fprintf(os.Stdout, "found %d files with functions\n", len(funcPoses))
 	}
 
-	// remember position for adding debug information
+	for file, positions := range funcPoses {
+		// sort from end to begin
+		sort.Slice(positions, func(i, j int) bool {
+			if positions[i].pos.Line == positions[j].pos.Line {
+				return positions[i].pos.Column < positions[j].pos.Column
+			}
+			return positions[i].pos.Line < positions[j].pos.Line
+		})
 
-	// TODO : add implementation
+		// read present file
+		dat, err := ioutil.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		if args.verbose {
+			fmt.Fprintln(os.Stdout, "inject debug information in file: ", file)
+		}
+
+		// inject function
+		lines := bytes.Split(dat, []byte("\n"))
+		for k := len(positions) - 1; k >= 0; k-- {
+
+			pos := positions[k].pos
+			if pos.Line-1 >= len(lines) {
+				return fmt.Errorf("try to add debug on outside of allowable line: %v", pos)
+			}
+			if pos.Column-1 >= len(lines[pos.Line-1]) {
+				return fmt.Errorf("try to add debug on outside of allowable column: %v", pos)
+			}
+			if lines[pos.Line-1][pos.Column-1] != '{' {
+				return fmt.Errorf("unacceptable char '{' : %c", lines[pos.Line-1][pos.Column-1])
+			}
+
+			lines[pos.Line-1] = append(lines[pos.Line-1][:pos.Column],
+				append([]byte(fmt.Sprintf("%s(%d,\"%s\");", debugFunctionName, pos.Line, positions[k].name)), lines[pos.Line-1][pos.Column:]...)...)
+		}
+
+		// add main debug function
+		lines = append([][]byte{[]byte(debugCode())}, lines...)
+
+		filename := file
+		// create a new filename
+		if index := strings.LastIndex(file, "/"); index >= 0 {
+			filename = file[:index+1] + args.debugPrefix + file[index+1:]
+		} else {
+			filename = args.debugPrefix + file
+		}
+
+		if args.verbose {
+			fmt.Fprintln(os.Stdout, "Write file with debug information in file: ", filename)
+		}
+
+		// save file with prefix+filename
+		err = ioutil.WriteFile(filename, bytes.Join(lines, []byte{'\n'}), 0644)
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
+
+const debugFunctionName string = "c4go_debug_function_name"
 
 func debugCode() string {
 	return `
