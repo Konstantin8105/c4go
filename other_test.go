a785789935afc14b6cdaa24a5bca8bdadeda0a4d
Konstantin8105 <konstantin8105@gmail.com>
2019-04-19 09:53:02 +0300

    fix test folder problem

--- other_test.go
+++ other_test.go
@@ -729,7 +729,16 @@ func TestTinn(t *testing.T) {
 	// wget http://archive.ics.uci.edu/ml/machine-learning-databases/semeion/semeion.data
 	index := strings.LastIndex(fileList[0], "/")
 	filepath := fileList[0][:index+1]
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
 	os.Chdir(filepath)
+	defer func() {
+		os.Chdir(currentDir)
+	}()
+
 	if err := downloadFile(filepath+"semeion.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/semeion/semeion.data"); err != nil {
 		t.Fatalf("%v", err)
 	}
