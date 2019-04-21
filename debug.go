35cb4b672e008437abe5e57de6bfca53b11b85cc
Konstantin8105 <konstantin8105@gmail.com>
2019-04-21 17:46:09 +0300

    step

--- debug.go
+++ debug.go
@@ -483,7 +483,9 @@ func (in *inj) walk(node ast.Node) {
 		in.newAllowablePosition(node.Position())
 		for i := 0; i < len(node.Children()); i++ {
 			size := len(in.varDecls)
-			in.walk(node.Children()[i])  // walking inside
+			fmt.Println(size)
+			in.walk(node.Children()[i]) // walking inside
+			fmt.Println(len(in.varDecls))
 			if size < len(in.varDecls) { // remove last VarDecls, if some added
 				in.varDecls = in.varDecls[:size]
 			}
