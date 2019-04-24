ddebdaa123307b064659e1761af9519059a69734
Konstantin8105 <konstantin8105@gmail.com>
2019-04-24 08:39:31 +0300

    add brainfuck

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -9,6 +9,9 @@ export VERIFICATION_FILE="./scripts/scripts.txt"
 
 echo "" > $OUTPUT_FILE
 
+./scripts/brainfuck.sh		2>&1 | grep -E 'warning|unsafe|Unsafe' | tee -a $OUTPUT_FILE
+echo "" >> $OUTPUT_FILE
+
 ./scripts/9wm.sh		2>&1 | grep -E 'warning|unsafe|Unsafe' | tee -a $OUTPUT_FILE
 echo "" >> $OUTPUT_FILE
 
