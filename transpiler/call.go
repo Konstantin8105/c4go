7f68354114e34a03bbb82a083fd7c3c90b3a8cd9
Konstantin8105 <konstantin8105@gmail.com>
2019-05-01 15:26:23 +0300

    fix 00219

--- transpiler/call.go
+++ transpiler/call.go
@@ -42,6 +42,12 @@ func getName(p *program.Program, firstChild ast.Node) (name string, err error) {
 	case *ast.DeclRefExpr:
 		return fc.Name, nil
 
+	case *ast.GenericSelectionExpr:
+		if len(fc.Children()) == 0 {
+			return undefineFunctionName, nil
+		}
+		return getName(p, fc.Children()[0])
+
 	case *ast.MemberExpr:
 		var expr goast.Expr
 		expr, _, _, _, err = transpileToExpr(fc, p, false)
