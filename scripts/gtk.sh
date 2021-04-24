489da589677393cb9d1fb0df979a1420a78c2bc9
Konstantin8105 <Konstantin8105@gmail.com>
2021-04-24 23:53:39 +0300

    add gtk scripts ast tree

--- scripts/gtk.sh
+++ scripts/gtk.sh
@@ -54,9 +54,13 @@ main (int    argc,
 	echo "***** remove go files"
 	rm -f $TEMP_FOLDER/*.go
 	rm -f $TEMP_FOLDER/*.app
+# ast 
+if [ "$1" == "-a" ]; then
+	$C4GO ast		-clang-flag="-pthread -I/usr/include/gtk-3.0 -I/usr/include/at-spi2-atk/2.0 -I/usr/include/at-spi-2.0 -I/usr/include/dbus-1.0 -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include -I/usr/include/gtk-3.0 -I/usr/include/gio-unix-2.0/ -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libpng16 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0"  \
+					 $CFILE
+fi
 
 # transpilation 
-export FILES=`ls $TEMP_FOLDER/*.c | tr "\n" " "`
 $C4GO transpile  -s           \
 	             -clang-flag="-pthread -I/usr/include/gtk-3.0 -I/usr/include/at-spi2-atk/2.0 -I/usr/include/at-spi-2.0 -I/usr/include/dbus-1.0 -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include -I/usr/include/gtk-3.0 -I/usr/include/gio-unix-2.0/ -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libpng16 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0"  \
 	             -o="$GOFILE" \
@@ -74,7 +78,7 @@ echo "Calculate warnings : $TEMP_FOLDER"
 	echo "		Unsafe   : $UNSAFE"
 # amount Go code lines
 	LINES=`wc $GOFILE`
-	echo "(lines,words,bytes)	 : $GOLINES"
+	echo "(lines,words,bytes)	 : $LINES"
 # defers
 	DEFER=`cat $GOFILE | grep "defer func" | wc -l`
 	echo "defer func           	 : $DEFER"
