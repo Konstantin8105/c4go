c0417dd82cb172b25c3de2d6d90bd55983b7104b
Konstantin8105 <konstantin8105@gmail.com>
2024-05-29 16:02:53 +0300

    step

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,6 +18,7 @@ echo "PKGS_DELIM : $PKGS_DELIM"
 go test                                 \
 				  -cover                \
 				  -timeout=30m          \
+                  -covermode=atomic     \
 	              -coverpkg=$PKGS_DELIM \
 				  -coverprofile=./testdata/pkg.coverprofile $PKGS
 
