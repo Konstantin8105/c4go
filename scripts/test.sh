dbdef3c84b26a20fb0825bafc1a732baff488103
Konstantin8105 <konstantin8105@gmail.com>
2024-05-30 09:58:33 +0300

    prepare script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-set -e
+go install github.com/ory/go-acc@latest
 
-echo "" > coverage.txt
+touch ./coverage.tmp
+echo 'mode: atomic' > coverage.txt
+go list ./... | grep -v /cmd | grep -v /vendor | xargs -n1 -I{} sh -c 'go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
 
-mkdir -p ./testdata/
 
-# Package list
-export PKGS=$(go list ./... | grep -v c4go/testdata | grep -v c4go/examples | grep -v c4go/tests | grep -v /vendor/ | tr '\n' ' ')
-
-# Make comma-separated.
-export PKGS_DELIM=$(echo "$PKGS" | tr ' ' ',')
-
-echo "PKGS       : $PKGS"
-echo "PKGS_DELIM : $PKGS_DELIM"
-
-go test                                 \
-				  -cover                \
-				  -timeout=30m          \
-	              -coverpkg=$PKGS_DELIM \
-				  -coverprofile=./testdata/pkg.coverprofile $PKGS
-
-# Merge coverage profiles.
-COVERAGE_FILES=`ls -1 ./testdata/*.coverprofile 2>/dev/null | wc -l`
-if [ $COVERAGE_FILES != 0 ]; then
-	# check program `gocovmerge` is exist
-	if which gocovmerge >/dev/null 2>&1; then
-		export FILES=$(ls testdata/*.coverprofile | tr '\n' ' ')
-		echo "Combine next coverprofiles : $FILES"
-		gocovmerge $FILES > coverage.txt
-	fi
-fi
+# set -e
+# 
+# echo "" > coverage.txt
+# 
+# mkdir -p ./testdata/
+# 
+# # Package list
+# export PKGS=$(go list ./... | grep -v c4go/testdata | grep -v c4go/examples | grep -v c4go/tests | grep -v /vendor/ | tr '\n' ' ')
+# 
+# # Make comma-separated.
+# export PKGS_DELIM=$(echo "$PKGS" | tr ' ' ',')
+# 
+# echo "PKGS       : $PKGS"
+# echo "PKGS_DELIM : $PKGS_DELIM"
+# 
+# go test                                 \
+# 				  -cover                \
+# 				  -timeout=30m          \
+# 	              -coverpkg=$PKGS_DELIM \
+# 				  -coverprofile=./testdata/pkg.coverprofile $PKGS
+# 
+# # Merge coverage profiles.
+# COVERAGE_FILES=`ls -1 ./testdata/*.coverprofile 2>/dev/null | wc -l`
+# if [ $COVERAGE_FILES != 0 ]; then
+# 	# check program `gocovmerge` is exist
+# 	if which gocovmerge >/dev/null 2>&1; then
+# 		export FILES=$(ls testdata/*.coverprofile | tr '\n' ' ')
+# 		echo "Combine next coverprofiles : $FILES"
+# 		gocovmerge $FILES > coverage.txt
+# 	fi
+# fi
 
 # echo "" > coverage.out
 # for d in $(go list ./... | grep -v vendor); do
@@ -41,4 +48,4 @@ fi
 #     fi
 # done
 
-echo "End of coverage"
+# echo "End of coverage"
