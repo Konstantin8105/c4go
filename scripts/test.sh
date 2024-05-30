c3746163f8d86257d2792e2c99f1d3e86c279209
Konstantin8105 <konstantin8105@gmail.com>
2024-05-30 11:55:32 +0300

    finilize tests script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# go install github.com/ory/go-acc@latest
-#
-# touch ./coverage.tmp
-# echo 'mode: atomic' > coverage.txt
-# go list ./... | grep -v /cmd | grep -v /vendor | xargs -n1 -I{} sh -c 'go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
-
-
-# set -e
-# 
-# echo "" > coverage.txt
-# 
-# mkdir -p ./testdata/
+set -e
 
 # github.com/Konstantin8105/c4go
 # github.com/Konstantin8105/c4go/ast
@@ -28,36 +17,18 @@
 # github.com/Konstantin8105/c4go/version
 
 # Package list
-# export PKGS="github.com/Konstantin8105/c4go github.com/Konstantin8105/c4go/ast github.com/Konstantin8105/c4go/noarch github.com/Konstantin8105/c4go/preprocessor github.com/Konstantin8105/c4go/program github.com/Konstantin8105/c4go/scripts github.com/Konstantin8105/c4go/transpiler github.com/Konstantin8105/c4go/types github.com/Konstantin8105/c4go/util github.com/Konstantin8105/c4go/version"
-export PKGS='github.com/Konstantin8105/c4go github.com/Konstantin8105/c4go/ast github.com/Konstantin8105/c4go/version'
-# export PKGS=$(go list -e ./... | grep -v testdata | grep -v examples | grep -v tests | grep -v vendor | tr '\n' ' ')
-
-# Make comma-separated.
-export PKGS_DELIM=$(echo "$PKGS" | tr ' ' ',')
-
+export PKGS=$(go list -e ./... | grep -v testdata | grep -v examples | grep -v tests | grep -v vendor | tr '\n' ' ')
+# View
 echo "PKGS       : $PKGS"
-echo "PKGS_DELIM : $PKGS_DELIM"
 
+# Initialize
+mkdir -p ./testdata/
+echo "" > coverage.txt
 touch ./coverage.tmp
+
+# Run tests
 echo 'mode: atomic' > coverage.txt
-go list -e ./...  | grep -v testdata | grep -v examples | grep -v tests | grep -v vendor | grep -v /cmd | grep -v /vendor | xargs -n100 -I{} sh -c 'go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
+echo $PKGS | xargs -n100 -I{} sh -c 'go test -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
  
-# go test \
-# 	-cover                \
-# 	-covermode=atomic     \
-# 	-timeout=30m          \
-# 	-coverpkg=$PKGS_DELIM \
-# 	-coverprofile=./testdata/pkg.coverprofile $PKGS
-
-# Merge coverage profiles.
-# COVERAGE_FILES=`ls -1 ./testdata/*.coverprofile 2>/dev/null | wc -l`
-# if [ $COVERAGE_FILES != 0 ]; then
-# 	# check program `gocovmerge` is exist
-# 	if which gocovmerge >/dev/null 2>&1; then
-# 		export FILES=$(ls testdata/*.coverprofile | tr '\n' ' ')
-# 		echo "Combine next coverprofiles : $FILES"
-# 		gocovmerge $FILES > coverage.txt
-# 	fi
-# fi
-
+# Finilize
 echo "End of coverage"
